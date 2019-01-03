45450440ff7dafa49451a64ac728fba54e1cf870
Oliver Wegner <void1976@gmail.com>
2019-01-03 16:44:30 +0100

    implement web service for heroku to run
    will call cljs code on http get to /

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-//require('./out/goog/bootstrap/nodejs.js');
-require('cljstest2');
-console.log(cljstest2);
-//require('./out/cljstest2/core.js');
-
+// load js stuff:
+const express = require('express');
 const Foo = require('foo');
 
-Foo.foo();
-cljstest2.core.bar();
-const r1 = cljstest2.baz.fun2(123);
-console.log(r1);
+
+// load cljs stuff:
+//require('./out/goog/bootstrap/nodejs.js');
+require('cljstest2');
+//require('./out/cljstest2/core.js');
+//console.log(cljstest2);
+
+
+// setup web app:
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    // call js stuff:
+    Foo.foo();
+
+    // call cljs stuff:
+    cljstest2.core.bar();
+    const r1 = cljstest2.baz.fun2(123);
+    console.log(r1);
+    res.send('Hello World!');
+});
+
+app.listen(
+    port,
+    () => console.log(`App listening on port ${port}.`)
+);
